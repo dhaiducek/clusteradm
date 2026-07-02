90d12b9e029c9cdae0b8b088ef46c725035ed682
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2026-07-02 17:22:10 -0400

    feat: add `raw` option for token fetch
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- pkg/cmd/get/token/exec.go
+++ pkg/cmd/get/token/exec.go
@@ -66,7 +66,9 @@ func (o *Options) writeResult(token, host string) error {
 		fmt.Println(o.Streams.Out, "token doesn't exist")
 		return nil
 	}
-	if o.output == "json" {
+
+	switch o.output {
+	case "json":
 		err := clusteradmjson.WriteJsonOutput(o.Streams.Out, clusteradmjson.HubInfo{
 			HubToken:     token,
 			HubApiserver: host,
@@ -74,9 +76,14 @@ func (o *Options) writeResult(token, host string) error {
 		if err != nil {
 			return err
 		}
-	} else {
+	case "text":
 		fmt.Fprintf(o.Streams.Out, "token=%s\n", token)
 		fmt.Fprintf(o.Streams.Out, "please log on spoke and run:\n%s join --hub-token %s --hub-apiserver %s --cluster-name <cluster_name>\n", helpers.GetExampleHeader(), token, host)
+	case "raw":
+		fmt.Fprintf(o.Streams.Out, "%s\n", token)
+	default:
+		return fmt.Errorf("invalid output format: %s", o.output)
 	}
+
 	return nil
 }
