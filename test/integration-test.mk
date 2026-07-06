ad6283b6eb124423849a06deecca0b30c17a7b51
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2026-07-06 17:19:46 -0400

    chore: use local bin rather than `/tmp` for CLIs
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- test/integration-test.mk
+++ test/integration-test.mk
@@ -1,20 +1,12 @@
 # Copyright Contributors to the Open Cluster Management project
-TEST_TMP :=/tmp
 
-export KUBEBUILDER_ASSETS ?=$(TEST_TMP)/kubebuilder/bin
-export GINKGO ?=$(TEST_TMP)/ginkgo/ginkgo
-
-ENSURE_ENVTEST_SCRIPT := https://raw.githubusercontent.com/open-cluster-management-io/sdk-go/main/ci/envtest/ensure-envtest.sh
-
+export KUBEBUILDER_ASSETS ?=$(LOCAL_BIN)/kubebuilder/bin
+export GINKGO ?=$(LOCAL_BIN)/ginkgo
 .PHONY: envtest-setup
-envtest-setup:
-	$(eval export KUBEBUILDER_ASSETS=$(shell curl -fsSL $(ENSURE_ENVTEST_SCRIPT) | bash))
-	@echo "KUBEBUILDER_ASSETS=$(KUBEBUILDER_ASSETS)"
-
 
 ensure-ginkgo:
-	$(info Downloading ginkgo into '$(TEST_TMP)/ginkgo')
-	GOBIN=$(TEST_TMP)/ginkgo go install github.com/onsi/ginkgo/v2/ginkgo@$(shell awk '/github.com\/onsi\/ginkgo\/v2/ {print $$2}' go.mod)
+	# Downloading ginkgo into '$(GINKGO)'
+	GOBIN=$(LOCAL_BIN) go install github.com/onsi/ginkgo/v2/ginkgo@$(shell awk '/github.com\/onsi\/ginkgo\/v2/ {print $$2}' go.mod)
 .PHONY: ensure-ginkgo
 
 clean-integration-test:
